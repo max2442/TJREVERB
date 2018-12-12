af4e4cc0ad4c55afb553e692cff3a12793cbb817
max2442 <43480994+max2442@users.noreply.github.com>
2018-12-12 15:54:36 -0500

    Update isis-imtq-core.py

--- isis-imtq-core.py
+++ isis-imtq-core.py
@@ -4,8 +4,34 @@ def k_adcs_terminate():
     ffi.cdef("""
         void k_adcs_terminate(void);
     """)
-    C = ffi.dlopen(None)
-    C.k_adcs_terminate()
+    C = ffi.verify("""
+        void k_adcs_terminate(void) {
+            const struct timespec MUTEX_TIMEOUT = {.tv_sec = 1, .tv_nsec = 0 };
+
+            /* Destroy the mutex */
+            if (pthread_mutex_timedlock(&imtq_mutex, &MUTEX_TIMEOUT) != 0)
+            {
+                perror("Failed to take MTQ mutex");
+                fprintf(stderr, "PID: %d TID: %ld", getpid(), syscall(SYS_gettid));
+            }
+            if (pthread_mutex_unlock(&imtq_mutex) != 0)
+            {
+                perror("Failed to unlock MTQ mutex");
+                fprintf(stderr, "PID: %d TID: %ld", getpid(), syscall(SYS_gettid));
+            }
+            if (pthread_mutex_destroy(&imtq_mutex) != 0)
+            {
+                perror("Failed to destroy MTQ mutex");
+                fprintf(stderr, "PID: %d TID: %ld", getpid(), syscall(SYS_gettid));
+            }
+
+            /* Close the I2C bus */
+            k_i2c_terminate(i2c_bus);
+
+            return;
+         }
+      """)
+     C.k_adcs_terminate()
 
 def k_imtq_watchdog_start():
     from cffi import FFI
@@ -13,7 +39,30 @@ def k_imtq_watchdog_start():
     ffi.cdef("""
         KADCSStatus k_imtq_watchdog_start(void);
     """)
-    C = ffi.dlopen(None)
+    C = ffi.verify("""
+        KADCSStatus k_imtq_watchdog_start(void) {
+            if (handle_watchdog != 0)
+            {
+                fprintf(stderr, "ADCS watchdog thread already started\n");
+                return ADCS_OK;
+            }
+            if (wd_timeout == 0)
+            {
+                fprintf(
+                    stderr,
+                    "ADCS watchdog has been disabled. No thread will be startd\n");
+                return ADCS_OK;
+            }
+            if (pthread_create(&handle_watchdog, NULL, kprv_imtq_watchdog_thread, NULL)
+                != 0)
+            {
+                perror("Failed to create ADCS watchdog thread");
+                handle_watchdog = 0;
+                return ADCS_ERROR;
+            }
+            return ADCS_OK;
+        }
+        """)
     C.k_imtq_watchdog_start()
     
 def k_imtq_watchdog_stop():
@@ -22,8 +71,26 @@ def k_imtq_watchdog_stop():
     ffi.cdef("""
         KADCSStatus k_imtq_watchdog_stop(void);
     """)
-    C = ffi.dlopen(None)
-    C.k_imtq_watchdog_stop()
+    C = ffi.verify("""
+        KADCSStatus k_imtq_watchdog_stop(void) {
+            if (pthread_cancel(handle_watchdog) != 0)
+            {
+                perror("Failed to cancel ADCS watchdog thread");
+                return ADCS_ERROR;
+            }
+            /* Wait for the cancellation to complete */
+            if (pthread_join(handle_watchdog, NULL) != 0)
+            {
+                perror("Failed to rejoin ADCS watchdog thread");
+                return ADCS_ERROR;
+            }
+
+            handle_watchdog = 0;
+
+            return ADCS_OK;
+            }
+            """)
+        C.k_imtq_watchdog_stop()
     
 def k_imtq_reset():
     from cffi import FFI
@@ -31,5 +98,11 @@ def k_imtq_reset():
     ffi.cdef("""
         KADCSStatus k_imtq_reset(void);
     """)
-    C = ffi.dlopen(None)
+    C = ffi.verify("""
+        KADCSStatus k_imtq_reset(void) {
+            return k_adcs_reset(SOFT_RESET);
+        }
+    """)
     C.k_imtq_reset()
+    
+    
